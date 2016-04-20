3ace3f6860bcf40a85d76b9d3cb52c2a7c23f1cd
benguariturk <benguariturk@gmail.com>
2016-04-20 20:55:39 +0300

    I don't wanna trouble

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace BenguTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bengü Ahmet'e kız ayarlasın");
+            Console.WriteLine("Hadi len!");
         }
     }
 }
