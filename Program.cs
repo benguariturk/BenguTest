ccc63eb18c27f78d9fafd72a546fd3f59f1876fa
benguariturk <benguariturk@gmail.com>
2016-04-20 21:32:45 +0300

    yeni değişiklik

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace BenguTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tamam lan fakir!");
+            Console.WriteLine("merhaba!");
         }
     }
 }
